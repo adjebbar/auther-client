34ec2ce76d32b80b53057f642ff198767935ba3e
adjebbar <45851643+adjebbar@users.noreply.github.com>
2020-10-20 17:04:02 +0200

    Add files via upload

---
+++ build/precache-manifest.ca007ff3e16a5fb80567d1600a6050f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aecb3102962b95ca756e1e208cda330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9536fbf0f01e0f6fef",
+    "url": "/static/css/main.0e715617.chunk.css"
+  },
+  {
+    "revision": "e559fc89a55aba67cc39",
+    "url": "/static/js/2.5208340e.chunk.js"
+  },
+  {
+    "revision": "e2346ec3998f678e386fd13bdddb7999",
+    "url": "/static/js/2.5208340e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b9536fbf0f01e0f6fef",
+    "url": "/static/js/main.fa2017c2.chunk.js"
+  },
+  {
+    "revision": "2eae13bc934d38dd9311",
+    "url": "/static/js/runtime-main.21e100e2.js"
+  }
+]);
